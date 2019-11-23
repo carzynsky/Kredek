8a0278c66e49519da7b1566fd9fcd41759632d01
Arek <Arek@DESKTOP-U4VRK31>
2019-11-23 21:25:47 +0100

    Zadanie domowe z lab1.

---
+++ lab1/ArkadiuszCarzynskiLab1Zadanie/FormMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArkadiuszCarzynskiLab1Zadanie
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStartGame_Click(object sender, EventArgs e)
+        {
+            if (textBoxUserName.Text.Trim() != string.Empty)
+            {
+                FormMain fM = new FormMain(textBoxUserName.Text);
+                fM.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nie podałeś imienia!");
+            }
+        }
+    }
+}
